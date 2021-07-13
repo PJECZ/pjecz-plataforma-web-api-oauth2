459af32f349fd3f3dbf3e866024f1cbbd35407ed
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-07-13 16:10:43 -0500

    Por probar cambios estructurales.

--- config/settings.py
+++ config/settings.py
@@ -5,12 +5,14 @@ import os
 
 
 # MySQL en Google Cloud
-DB_USER = os.environ.get("DB_USER", "nouser")
-DB_PASS = os.environ.get("DB_PASS", "wrongpassword")
+DB_USER = os.environ.get("DB_USER", "wronguser")
+DB_PASS = os.environ.get("DB_PASS", "badpassword")
 DB_NAME = os.environ.get("DB_NAME", "pjecz_plataforma_web")
 DB_SOCKET_DIR = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
 CLOUD_SQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME", "none")
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket={DB_SOCKET_DIR}/{CLOUD_SQL_CONNECTION_NAME}"
 
-# Always in False
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# OAuth2
+SECRET_KEY = os.environ.get("SECRET_KEY") # openssl rand -hex 32
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
--- plataforma_web/usuarios/schemas.py
+++ plataforma_web/usuarios/schemas.py
@@ -2,6 +2,7 @@
 Usuarios, esquemas de pydantic
 """
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Usuario(BaseModel):
@@ -17,4 +18,23 @@ class Usuario(BaseModel):
     email: str
     nombres: str
     apellido_paterno: str
-    apellido_materno: str
+    apellido_materno: Optional[str] = None
+
+
+class UsuarioEnBD(Usuario):
+    """Usuario en BD"""
+
+    hashed_password: str
+
+
+class Token(BaseModel):
+    """Token"""
+
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    """Token data"""
+
+    username: Optional[str] = None
