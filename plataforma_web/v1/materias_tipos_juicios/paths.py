500dd984e8e16b1baf61717716618233e0949635
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-09-24 14:53:51 -0500

    Sentencias por tipo de juicio.

--- plataforma_web/v1/materias_tipos_juicios/paths.py
+++ plataforma_web/v1/materias_tipos_juicios/paths.py
@@ -13,6 +13,8 @@ from plataforma_web.v1.usuarios.schemas import UsuarioInBD
 
 from plataforma_web.v1.materias_tipos_juicios.crud import get_materias_tipos_juicios, get_materia_tipo_juicio
 from plataforma_web.v1.materias_tipos_juicios.schemas import MateriaTipoJuicioOut
+from plataforma_web.v1.sentencias.crud import get_sentencias
+from plataforma_web.v1.sentencias.schemas import SentenciaOut
 
 materias_tipos_juicios = APIRouter(prefix="/v1/materias", tags=["materias"])
 
@@ -47,8 +49,32 @@ async def detalle_materia_tipo_juicio(
         raise HTTPException(status_code=403, detail="Forbidden")
     try:
         materia_tipo_juicio = get_materia_tipo_juicio(db, materia_tipo_juicio_id=tipo_juicio_id)
+        if materia_tipo_juicio.materia_id != materia_id:
+            raise ValueError("No corresponde la materia al tipo de juicio")
     except IndexError as error:
         raise HTTPException(status_code=404, detail=f"Not found: {str(error)}") from error
     except ValueError as error:
         raise HTTPException(status_code=406, detail=f"Not acceptable: {str(error)}") from error
     return MateriaTipoJuicioOut.from_orm(materia_tipo_juicio)
+
+
+@materias_tipos_juicios.get("/{materia_id}/tipos_juicios/{tipo_juicio_id}/sentencias", response_model=LimitOffsetPage[SentenciaOut])
+async def listado_materias_tipos_juicios_sentencias(
+    materia_id: int,
+    tipo_juicio_id: int,
+    current_user: UsuarioInBD = Depends(get_current_active_user),
+    db: Session = Depends(get_db),
+):
+    """Listado de sentencias de un tipo de juicio"""
+    if not current_user.permissions & Permiso.VER_JUSTICIABLES == Permiso.VER_JUSTICIABLES:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    try:
+        materia_tipo_juicio = get_materia_tipo_juicio(db, tipo_juicio_id)
+        if materia_tipo_juicio.materia_id != materia_id:
+            raise ValueError("No corresponde la materia al tipo de juicio")
+        listado = get_sentencias(db, materia_tipo_juicio_id=tipo_juicio_id)
+    except IndexError as error:
+        raise HTTPException(status_code=404, detail=f"Not found: {str(error)}") from error
+    except ValueError as error:
+        raise HTTPException(status_code=406, detail=f"Not acceptable: {str(error)}") from error
+    return paginate(listado)
