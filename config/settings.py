b14aede7889bbd02f21fd38100555bede2d4d71b
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-12-05 22:12:32 -0600

    Cambio a Justicia Digital gob mx.

--- config/settings.py
+++ config/settings.py
@@ -4,15 +4,22 @@ Configuración para producción
 import os
 
 
-DB_USER = os.environ.get("DB_USER", "wronguser")
-DB_PASS = os.environ.get("DB_PASS", "badpassword")
-DB_NAME = os.environ.get("DB_NAME", "pjecz_plataforma_web")
+# Google Cloud SQL
 DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
+DB_NAME = os.environ.get("DB_NAME", "pjecz_plataforma_web")
+DB_PASS = os.environ.get("DB_PASS", "wrongpassword")
+DB_USER = os.environ.get("DB_USER", "nouser")
 DB_SOCKET_DIR = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-
-# MySQL en Google Cloud
 CLOUD_SQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME", "none")
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket={DB_SOCKET_DIR}/{CLOUD_SQL_CONNECTION_NAME}"
+
+# DESHABILITADO Google Cloud SQL a Ceres con MySQL
+# SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket={DB_SOCKET_DIR}/{CLOUD_SQL_CONNECTION_NAME}"
+
+# NUEVO Google Cloud SQL a Minerva con PostgreSQL
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+
+# Always in False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # OAuth2
 SECRET_KEY = os.environ.get("SECRET_KEY") # openssl rand -hex 32
