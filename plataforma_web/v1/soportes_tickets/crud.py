92c5433e4c6b382a1beedf7e243a0eff7bd0fd2d
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-02-27 17:01:52 -0600

    Se han mejorado los reportes.

--- plataforma_web/v1/soportes_tickets/crud.py
+++ plataforma_web/v1/soportes_tickets/crud.py
@@ -4,11 +4,14 @@ Soportes Tickets v1, CRUD (create, read, update, and delete)
 from datetime import date
 from typing import Any
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 
 from lib.safe_string import safe_string
 
 from plataforma_web.v1.oficinas.crud import get_oficina, get_oficina_from_clave
+from plataforma_web.v1.oficinas.models import Oficina
 from plataforma_web.v1.soportes_categorias.crud import get_soporte_categoria
+from plataforma_web.v1.soportes_categorias.models import SoporteCategoria
 from plataforma_web.v1.soportes_tickets.models import SoporteTicket
 from plataforma_web.v1.usuarios.crud import get_usuario, get_usuario_from_email
 from plataforma_web.v1.usuarios.models import Usuario
@@ -72,3 +75,35 @@ def get_soporte_ticket(db: Session, soporte_ticket_id: int) -> SoporteTicket:
     if soporte_ticket.estatus != "A":
         raise ValueError("No es activo ese ticket, está eliminado")
     return soporte_ticket
+
+
+def get_total_by_oficina_and_categoria(
+    db: Session,
+    estado: str = None,
+    creado_desde: date = None,
+    creado_hasta: date = None,
+) -> Any:
+    """Consultar totales de tickets por oficina y por categoria"""
+    consulta = db.\
+        query(
+            func.substring(Oficina.clave, 1, 4).label("distrito_clave"),
+            SoporteCategoria.nombre.label("soporte_categoria_nombre"),
+            func.count("*").label("cantidad"),
+        ).\
+        select_from(SoporteTicket).join(Usuario).join(Oficina).join(SoporteCategoria)
+    estado = safe_string(estado)
+    if estado:
+        if estado not in SoporteTicket.ESTADOS:
+            raise ValueError("Estado incorrecto")
+        consulta = consulta.filter(SoporteTicket.estado == estado)
+    if creado_desde:
+        if not date(year=2000, month=1, day=1) <= creado_desde <= date.today():
+            raise ValueError("Fecha fuera de rango")
+        consulta = consulta.filter(SoporteTicket.creado >= creado_desde)
+    if creado_hasta:
+        if not date(year=2000, month=1, day=1) <= creado_hasta <= date.today():
+            raise ValueError("Fecha fuera de rango")
+        consulta = consulta.filter(SoporteTicket.creado <= creado_hasta)
+    return consulta.\
+        order_by("distrito_clave", "soporte_categoria_nombre").\
+        group_by("distrito_clave", "soporte_categoria_nombre")
--- plataforma_web/v1/soportes_tickets/paths.py
+++ plataforma_web/v1/soportes_tickets/paths.py
@@ -3,6 +3,7 @@ Soportes Tickets v1, rutas (paths)
 """
 from datetime import date
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi_pagination import Page
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy.orm import Session
 
@@ -10,8 +11,8 @@ from lib.database import get_db
 from lib.fastapi_pagination import LimitOffsetPage
 
 from plataforma_web.v1.permisos.models import Permiso
-from plataforma_web.v1.soportes_tickets.crud import get_soportes_tickets, get_soporte_ticket
-from plataforma_web.v1.soportes_tickets.schemas import SoporteTicketOut
+from plataforma_web.v1.soportes_tickets.crud import get_soportes_tickets, get_soporte_ticket, get_total_by_oficina_and_categoria
+from plataforma_web.v1.soportes_tickets.schemas import SoporteTicketOut, SoporteTicketTotalOut
 from plataforma_web.v1.usuarios.authentications import get_current_active_user
 from plataforma_web.v1.usuarios.schemas import UsuarioInDB
 
@@ -47,6 +48,31 @@ async def listado_soportes_tickets(
             creado_desde=creado_desde,
             creado_hasta=creado_hasta,
             descripcion=descripcion,
+        )
+    except IndexError as error:
+        raise HTTPException(status_code=404, detail=f"Not found: {str(error)}") from error
+    except ValueError as error:
+        raise HTTPException(status_code=406, detail=f"Not acceptable: {str(error)}") from error
+    return paginate(listado)
+
+
+@soportes_tickets.get("/totales", response_model=Page[SoporteTicketTotalOut])
+async def listado_totales_soportes_tickets(
+    estado: str = None,
+    creado_desde: date = None,
+    creado_hasta: date = None,
+    current_user: UsuarioInDB = Depends(get_current_active_user),
+    db: Session = Depends(get_db),
+):
+    """Listado de totales de tickets por oficina y por categoria"""
+    if "SOPORTES TICKETS" not in current_user.permissions or current_user.permissions["SOPORTES TICKETS"] < Permiso.VER:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    try:
+        listado = get_total_by_oficina_and_categoria(
+            db,
+            estado=estado,
+            creado_desde=creado_desde,
+            creado_hasta=creado_hasta,
         )
     except IndexError as error:
         raise HTTPException(status_code=404, detail=f"Not found: {str(error)}") from error
