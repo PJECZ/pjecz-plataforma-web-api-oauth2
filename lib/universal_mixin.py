98f00305f108a9359fd4685c646af80504054cd8
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-27 07:09:51 -0500

    Por probar

--- lib/universal_mixin.py
+++ lib/universal_mixin.py
@@ -1,11 +1,36 @@
 """
 UniversalMixin define las columnas y métodos comunes de todos los modelos
 """
+import os
+import re
+from hashids import Hashids
 from sqlalchemy import Column, DateTime, String
+from sqlalchemy.sql import func
+
+HASHID_REGEXP = re.compile("[0-9a-zA-Z]{8,16}")
+SALT = os.environ.get("SALT", "Esta es una muy mala cadena aleatoria")
+
+hashids = Hashids(salt=SALT, min_length=8)
 
 
 class UniversalMixin:
     """Columnas y métodos comunes a todas las tablas"""
 
     creado = Column(DateTime, nullable=False)
+    modificado = Column(DateTime, onupdate=func.now(), server_default=func.now())
     estatus = Column(String(1), server_default="A", nullable=False)
+
+    def encode_id(self):
+        """Convertir el ID de entero a cadena"""
+        return hashids.encode(self.id)
+
+    @classmethod
+    def decode_id(cls, id_encoded: str):
+        """Convertir el ID de entero a cadena"""
+        if re.fullmatch(HASHID_REGEXP, id_encoded) is None:
+            return None
+        descifrado = hashids.decode(id_encoded)
+        try:
+            return descifrado[0]
+        except IndexError:
+            return None
--- plataforma_web/v1/centros_trabajos/paths.py
+++ plataforma_web/v1/centros_trabajos/paths.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from lib.database import get_db
 from lib.fastapi_pagination import LimitOffsetPage
 
-from plataforma_web.v1.centros_trabajos.crud import get_centro_trabajo_from_clave, get_centros_trabajos
+from plataforma_web.v1.centros_trabajos.crud import get_centro_trabajo, get_centro_trabajo_from_clave, get_centros_trabajos
 from plataforma_web.v1.centros_trabajos.schemas import CentroTrabajoOut
 from plataforma_web.v1.permisos.models import Permiso
 from plataforma_web.v1.usuarios.authentications import get_current_active_user
@@ -28,9 +28,9 @@ async def listado_centros_trabajos(
     return paginate(get_centros_trabajos(db))
 
 
-@centros_trabajos.get("/{clave}", response_model=CentroTrabajoOut)
-async def detalle_centro_trabajo_con_clave(
-    clave: str,
+@centros_trabajos.get("/{centro_trabajo_id}", response_model=CentroTrabajoOut)
+async def detalle_centro_trabajo(
+    centro_trabajo_id: int,
     current_user: UsuarioInDB = Depends(get_current_active_user),
     db: Session = Depends(get_db),
 ):
@@ -38,7 +38,7 @@ async def detalle_centro_trabajo_con_clave(
     if "CENTROS TRABAJOS" not in current_user.permissions or current_user.permissions["CENTROS TRABAJOS"] < Permiso.VER:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
     try:
-        centro_trabajo = get_centro_trabajo_from_clave(db, clave=clave)
+        centro_trabajo = get_centro_trabajo(db, centro_trabajo_id=centro_trabajo_id)
     except IndexError as error:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found: {str(error)}") from error
     except ValueError as error:
