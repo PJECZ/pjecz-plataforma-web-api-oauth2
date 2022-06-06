870012580d4ed6fe4d47c74b0a3f214d78dad0e9
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-06 13:40:29 -0500

    Username en token

--- plataforma_web/app.py
+++ plataforma_web/app.py
@@ -97,7 +97,11 @@ async def ingresar_para_solicitar_token(form_data: OAuth2PasswordRequestForm = D
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": usuario.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "username": usuario.username,
+    }
 
 
 @app.get("/profile", response_model=UsuarioInDB)
