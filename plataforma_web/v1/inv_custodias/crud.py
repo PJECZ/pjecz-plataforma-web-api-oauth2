66cdb08de70745081f50b4df0650170a4892203c
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-14 16:34:52 -0500

    Categroias

--- plataforma_web/v1/inv_custodias/crud.py
+++ plataforma_web/v1/inv_custodias/crud.py
@@ -1,18 +1,30 @@
 """
 Inventarios Custodias v1, CRUD (create, read, update, and delete)
 """
+from datetime import date
 from typing import Any
 from sqlalchemy.orm import Session
 
 from .models import InvCustodia
+from ..usuarios.crud import get_usuario
 
 
 def get_inv_custodias(
     db: Session,
+    usuario_id: int = None,
+    fecha_desde: date = None,
+    fecha_hasta: date = None,
 ) -> Any:
     """Consultar los custodias activos"""
     consulta = db.query(InvCustodia)
-    return consulta.filter_by(estatus="A").order_by(InvCustodia.id)
+    if usuario_id:
+        usuario = get_usuario(db, usuario_id=usuario_id)
+        consulta = consulta.filter(InvCustodia.usuario == usuario)
+    if fecha_desde:
+        consulta = consulta.filter(InvCustodia.fecha >= fecha_desde)
+    if fecha_hasta:
+        consulta = consulta.filter(InvCustodia.fecha <= fecha_hasta)
+    return consulta.filter_by(estatus="A").order_by(InvCustodia.id.desc())
 
 
 def get_inv_custodia(db: Session, inv_custodia_id: int) -> InvCustodia:
