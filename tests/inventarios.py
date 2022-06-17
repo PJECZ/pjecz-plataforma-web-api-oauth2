0b9ed1c2dc4952ff6648a9f6872515fe791a3539
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-17 17:22:18 -0500

    Ya usa pandas

--- tests/inventarios.py
+++ tests/inventarios.py
@@ -23,12 +23,14 @@ PASSWORD = os.getenv("PASSWORD")
 
 def authenticate():
     """Autentificarse y obtener el token"""
+    if BASE_URL is None or USERNAME is None or PASSWORD is None:
+        raise Exception("Error de configuracion.")
     data = {"username": USERNAME, "password": PASSWORD}
     headers = {"content-type": "application/x-www-form-urlencoded"}
     response = requests.post(f"{BASE_URL}/token", data=data, headers=headers)
-    if response.status_code == 200:
-        return response.json()["access_token"]
-    return None
+    if response.status_code != 200:
+        raise requests.HTTPError(response.status_code)
+    return response.json()["access_token"]
 
 
 def get_matriz(authorization_header):
@@ -39,28 +41,44 @@ def get_matriz(authorization_header):
         raise error
     if response.status_code != 200:
         raise requests.HTTPError(response.status_code)
-    return response.json()
+    data_json = response.json()
+    dataframe = pd.json_normalize(data_json)
+    return dataframe
 
 
-def main():
-    """Main function"""
-    # Authenticate
-    token = authenticate()
-    if token is None:
-        print("Error al autenticarse")
-        return
-    authorization_header = {"Authorization": "Bearer " + token}
-    # Matriz
-    matriz = get_matriz(authorization_header)
-    dataframe = pd.json_normalize(matriz)
+def get_cantidades_oficina_tipo(authorization_header):
+    """Obtener las cantidades de equipos por oficina y por tipo"""
+    try:
+        response = requests.get(f"{BASE_URL}/v1/inv_equipos/cantidades_oficina_tipo", headers=authorization_header, timeout=12)
+    except requests.exceptions.RequestException as error:
+        raise error
+    if response.status_code != 200:
+        raise requests.HTTPError(response.status_code)
+    data_json = response.json()
+    dataframe = pd.json_normalize(data_json)
     dataframe.oficina_clave = dataframe.oficina_clave.astype("category")
     dataframe.inv_equipo_tipo = dataframe.inv_equipo_tipo.astype("category")
-    print(dataframe.info())
     # reporte = dataframe.pivot_table(
     #     index=["oficina_clave"],
     #     columns=["inv_equipo_tipo"],
     #     values="cantidad",
     # )
+    return dataframe
+
+
+def main():
+    """Main function"""
+    try:
+        token = authenticate()
+        authorization_header = {"Authorization": "Bearer " + token}
+        # matriz = get_matriz(authorization_header)
+        # print(matriz)
+        cantidades_oficina_tipo = get_cantidades_oficina_tipo(authorization_header)
+        print(str(cantidades_oficina_tipo))
+    except requests.HTTPError as error:
+        print("Error de comunicacion " + str(error))
+    except Exception as error:
+        print(str(error))
 
 
 if __name__ == "__main__":
