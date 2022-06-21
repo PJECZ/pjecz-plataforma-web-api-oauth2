f0871182448911c4a713b29bb475655a929588dc
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-21 15:44:59 -0500

    Agregue openpyxl a setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="plataforma_web",
     version="0.1",
     packages=["plataforma_web"],
-    install_requires=["click", "pandas", "requests", "sendgrid", "tabulate"],
+    install_requires=["click", "openpyxl", "pandas", "requests", "sendgrid", "tabulate"],
     entry_points={
         "console_scripts": [
             "plataforma_web = cli.cli:cli",
