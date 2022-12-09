b2849a05aa39517ee7a11ea23f011a79d7536c25
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-12-09 16:06:15 -0600

    Modelos listos

---
+++ plataforma_web/v1/repsvm_delitos/models.py
@@ -0,0 +1,28 @@
+"""
+REPSVM Delitos v1, modelos
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from lib.database import Base
+from lib.universal_mixin import UniversalMixin
+
+
+class REPSVMDelitos(Base, UniversalMixin):
+    """REPSVMDelitos"""
+
+    # Nombre de la tabla
+    __tablename__ = "repsvm_delitos"
+
+    # Clave primaria
+    id = Column(Integer, primary_key=True)
+
+    # Columnas
+    nombre = Column(String(256), unique=True, nullable=False)
+
+    # Hijos
+    repsvm_agresores_delitos = relationship("REPSVMAgresorDelito", back_populates="repsvm_delito")
+
+    def __repr__(self):
+        """Representación"""
+        return f"<REPSVMDelitos {self.id}>"
